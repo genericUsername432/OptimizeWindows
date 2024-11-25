e04f7c5842d21b3d9943e23acfdad7ee79deb965
genericUsername432 <tenzinolul@gmail.com>
2024-11-26 01:04:29 +0530

    New Features Added run.ps1
    Disable tracking features, Disable hibernation, Disable Windows Consumer features

--- Documents/PowerShellScripts/OptimizeWindows/run.ps1
+++ Documents/PowerShellScripts/OptimizeWindows/run.ps1
@@ -4,6 +4,7 @@ $run = Read-Host -Prompt "RUN ? (Y/N)"
 
 
 if ("Y" -eq $run)
+
 {
 
     echo "Running Tweaks"
@@ -11,12 +12,12 @@ if ("Y" -eq $run)
 
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0 -Force
     Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" | Select-Object AllowTelemetry
-
+    # Clear Temp Folder
     $tempPath = $env:TEMP
     Get-ChildItem -Path $tempPath -Recurse -Force | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
     Write-Host "Temp folder cleared: $tempPath" -ForegroundColor Green
 
-    # Empty the Recycle Bin
+    # Empty The Recycle Bin
     Clear-RecycleBin -Force
 
     # Confirmation Message
@@ -27,8 +28,21 @@ if ("Y" -eq $run)
     #Use 1.1.1.1 And 8.8.8.8 As Default DNS
 
     Set-DnsClientServerAddress -InterfaceAlias Wi-Fi -ServerAddresses "1.1.1.1","8.8.8.8"
-
     
+    #Disable Windows Consumer Features
+
+    Set-ItemProperty -Path "HKLM:\\SOFTWARE\\Policies\\Microsoft\\Windows\\CloudContent" -Name "DisableWindowsConsumerFeatures" -Value 1 -Force
+    
+    #Disable Hibernation ( For PC only)
+
+    powercfg.exe /hibernate off
+
+    #Disable Tracking Features
+
+    Set-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\location" -Name "Value" -Value "Deny" -Force
+    Set-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Sensor\\Overrides\\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Value "0" -Force
+    Set-ItemProperty -Path "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\lfsvc\\Service\\Configuration" -Name "Status" -Value "0" -Force
+    Set-ItemProperty -Path "HKLM:\\SYSTEM\\Maps" -Name "Value" -Value "0" -Force
 
 
 }
